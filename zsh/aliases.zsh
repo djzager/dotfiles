09edd9aa4c670a8ce24cf14d7572831c2959fe72
David Zager <david.j.zager@gmail.com>
2018-06-25 23:32:29 -0400

    Updates for media keys in i3

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,9 @@
 # Hide/show all desktop icons (useful when presenting)
-alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
-alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
-
 alias :q='exit'
+alias dswap='find . -type f -name "*.sw[klmnop]" -delete'
 
 alias asciicast2gif='docker run --rm -v $PWD:/data:Z asciinema/asciicast2gif'
+alias apbrun='docker run --net=host -v ~/.kube:/opt/apb/.kube:z -u $UID'
+
+# Alias to vimx only if installed
+command -v vimx >/dev/null 2>&1 && alias vim='vimx'
