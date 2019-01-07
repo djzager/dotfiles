ecc75d64c7f2113014db81c6c846641f1c7a3853
David Zager <david.j.zager@gmail.com>
2019-01-07 16:17:55 -0500

    Add gnome-terminal profile scripts

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,6 +2,7 @@
 alias :q='exit'
 alias dswap='find . -type f -name "*.sw[klmnop]" -delete'
 alias k='kubectl'
+alias gpg='gpg2'
 
 alias asciicast2gif='docker run --rm -v $PWD:/data:Z asciinema/asciicast2gif'
 alias apbrun='docker run --net=host -v ~/.kube:/opt/apb/.kube:z -u $UID'
--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -6,4 +6,6 @@ export GOROOT=$(go env GOROOT)
 export GOPATH=$HOME/go
 export WORKSPACE=$HOME/Workspace
 export KEYTIMEOUT=1
+
+# https://wiki.archlinux.org/index.php/Qt#Configuration_of_Qt5_apps_under_environments_other_than_KDE_Plasma
 export QT_QPA_PLATFORMTHEME="qt5ct"
